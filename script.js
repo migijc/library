343c444548b0679b4bad0a89f08a4a932bab681d
Miguel <migijc@gmail.com>
2022-06-17 20:21:05 -0400

    Added button to get user to be able to input book details

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Global variables
+const body= document.querySelector("body");
+
+
 function Books(title, author, pages, read){
     this.bookName=title
     this.bookAuthor=author
@@ -8,4 +12,49 @@ function Books(title, author, pages, read){
     }
 }
 
-let newBook= new Books("Hello World", "miguel Gil", 350, "not read")
+let myLibrary=[];
+
+
+function addBookToLibrary(){
+    let getBookName=prompt("Name of book?")
+    let getBookAuthor=prompt("Who is the author?")
+    let getBookPages=prompt("How many pages does it have?")
+    let hasBeenRead=prompt("You read this book true or false?")
+    let newBook= new Books(getBookName, getBookAuthor, getBookPages, hasBeenRead)
+    myLibrary.push(newBook);
+}
+// addBookToLibrary();
+
+let bookNameDisplay= myLibrary.forEach((item) => {
+    let display= document.createElement("p");
+    display.textContent=item.bookName;
+    body.appendChild(display);
+
+})
+
+const newBookButton= document.querySelector(".newBook")
+
+newBookButton.addEventListener('click', (e) =>{
+    let form= document.createElement('form')
+    let formGetBookName=document.createElement('input')
+    let formGetBookAuthor=document.createElement('input')
+    let formGetBookPages=document.createElement('input')
+    let formHasRead=document.createElement('input')
+    let formSubmit=document.createElement("button")
+    formSubmit.setAttribute("type", "button")
+    form.appendChild(formGetBookName)
+    form.appendChild(formGetBookAuthor)
+    form.appendChild(formGetBookPages)
+    form.appendChild(formHasRead)
+    form.appendChild(formSubmit)
+    body.appendChild(form)
+    formSubmit.addEventListener("click", () => {
+        console.log(formGetBookAuthor.value)
+    })
+
+})
+
+
+
+
+
