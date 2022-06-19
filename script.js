01048e2bf1f5bbc437c2c6582b6a670ad3fe56f2
Miguel <migijc@gmail.com>
2022-06-18 23:34:17 -0400

    added a but of UI, changed form display, fixed issue with bookcard displaying several times

--- script.js
+++ script.js
@@ -3,57 +3,71 @@ const body= document.querySelector("body");
 
 
 function Books(title, author, pages, read){
-    this.bookName=title
-    this.bookAuthor=author
-    this.bookPages=pages
-    this.bookRead=read
+    this.bookName=title;
+    this.bookAuthor=author;
+    this.bookPages=pages;
+    this.bookRead=read;
     this.info= function(){
-        return(title + " by " + author+", " + pages +" pages"+", " + read)
-    }
-}
+        return(title + " by " + author+", " + pages +" pages"+", " + read);
+    };
+};
 
 let myLibrary=[];
 
 
 function addBookToLibrary(){
-    let getBookName=prompt("Name of book?")
-    let getBookAuthor=prompt("Who is the author?")
-    let getBookPages=prompt("How many pages does it have?")
-    let hasBeenRead=prompt("You read this book true or false?")
-    let newBook= new Books(getBookName, getBookAuthor, getBookPages, hasBeenRead)
+    let getBookName=prompt("Name of book?");
+    let getBookAuthor=prompt("Who is the author?");
+    let getBookPages=prompt("How many pages does it have?");
+    let hasBeenRead=prompt("You read this book true or false?");
+    let newBook= new Books(getBookName, getBookAuthor, getBookPages, hasBeenRead);
     myLibrary.push(newBook);
-}
-// addBookToLibrary();
+};
+let formGetBookName=document.querySelector('#bookName');
+let formGetBookAuthor=document.querySelector('#bookAuthor');
+let formSubmit=document.querySelector(".addBook");
+const newBookButton= document.querySelector(".newBook");
+const theForm= document.querySelector(".theForm")
+newBookButton.addEventListener('click', () =>{
+    theForm.classList.add("showNow")
+});
 
-let bookNameDisplay= myLibrary.forEach((item) => {
-    let display= document.createElement("p");
-    display.textContent=item.bookName;
-    body.appendChild(display);
+formSubmit.addEventListener("click", () => {
+    let newBook= new Books(formGetBookName.value, formGetBookAuthor.value);
+    myLibrary.push(newBook);
+    theForm.classList.remove("showNow")
+    displayBookNames()
+});
 
-})
+let booksGrid= document.createElement("div");
 
-const newBookButton= document.querySelector(".newBook")
+let displayBookNames = function (){
+    let bookCard= document.createElement("div");
+    let titleToDisplay=document.createElement('h4');
+    titleToDisplay.classList.add("titleToDisplay")
+    let authorToDisplay=document.createElement("h4");
+    let pagesToDisplay= document.createElement('h4');
+    let hasReadToDisplay=document.createElement('h4')
+    bookCard.classList.add("bookCards")
+    body.appendChild(booksGrid)
+    let bookNameDisplay= myLibrary.forEach((item) => {
+        titleToDisplay.textContent=item.bookName
+        authorToDisplay.textContent= `By: ${item.bookAuthor}`;
+        pagesToDisplay.textContent=`Pages: ${item.bookPages}`;
+        hasReadToDisplay.textContent=item.bookRead;
+    });
+    // booksGrid.appendChild(titleToDisplay)
+    // booksGrid.appendChild(authorToDisplay)
+    // booksGrid.appendChild(pagesToDisplay)
+    // booksGrid.appendChild(hasReadToDisplay)
+        booksGrid.classList.add("showing")
+        booksGrid.appendChild(bookCard)
+        bookCard.appendChild(titleToDisplay)
+        bookCard.appendChild(authorToDisplay)
+        bookCard.appendChild(pagesToDisplay)
+        bookCard.appendChild(hasReadToDisplay);
 
-newBookButton.addEventListener('click', (e) =>{
-    let form= document.createElement('form')
-    let formGetBookName=document.createElement('input')
-    let formGetBookAuthor=document.createElement('input')
-    let formGetBookPages=document.createElement('input')
-    let formHasRead=document.createElement('input')
-    let formSubmit=document.createElement("button")
-    formSubmit.setAttribute("type", "button")
-    form.appendChild(formGetBookName)
-    form.appendChild(formGetBookAuthor)
-    form.appendChild(formGetBookPages)
-    form.appendChild(formHasRead)
-    form.appendChild(formSubmit)
-    body.appendChild(form)
-    formSubmit.addEventListener("click", () => {
-        console.log(formGetBookAuthor.value)
-    })
-
-})
-
+};
 
 
 
